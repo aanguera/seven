55eba8659619c8d410161f2fb89437ecd2cea89b
Toni Anguera <aanguera.65@gmail.com>
2014-11-11 18:21:36 +0100

    Afegeixo la resta de números fins el 9 que faltaven

--- seven/seven.ino
+++ seven/seven.ino
@@ -21,11 +21,18 @@ int seg_g = 8;
 
 const char segmentPins[7] = { seg_a,seg_b,seg_c,seg_d,seg_e,seg_f,seg_g};
 
-const byte numLeds[3] = {
+const byte numLeds[10] = {
   //DP GFEDCBA  Ordre dels segments
   B00111111,  // Representa el 0
   B00000110,  // Representa el 1
-  B01011011,
+  B01011011,  // Representa el 2
+  B01001111,  // Representa el 3
+  B01100110,  // Representa el 4
+  B01101101,  // Representa el 5
+  B01111100,  // Representa el 6
+  B00000111,  // Representa el 7
+  B01111111,  // Representa el 8
+  B01100111,  // Representa el 9
 };
 
 //******  Includes  ******************************************************************
@@ -70,7 +77,7 @@ void loop() {
   delay(1000);
 */ 
   // representa el 0
-  for(int numero = 0; numero < 3; numero++)
+  for(int numero = 0; numero < 10; numero++)
   {
     for(int segment = 0; segment < 7; segment++)
     {
